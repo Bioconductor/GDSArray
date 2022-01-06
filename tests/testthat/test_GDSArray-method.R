7f953ac6329dde010ad9014a82deff320e0f7593
Qian Liu <qliu7@buffalo.edu>
2022-01-06 16:45:25 -0500

    fix check error on windows

--- tests/testthat/test_GDSArray-method.R
+++ tests/testthat/test_GDSArray-method.R
@@ -1,7 +1,7 @@
 test_that("gdsfile getter and setter works", {
     file <- gdsExampleFileName("snpgds")
     ga <- GDSArray(file, "genotype")
-    expect_equal(gdsfile(ga), file)
+    expect_equal(normalizePath(gdsfile(ga)), normalizePath(file))
     file1 <- gdsExampleFileName("seqgds")
     gdsfile(ga) <- file1
     expect_true(validObject(ga))
